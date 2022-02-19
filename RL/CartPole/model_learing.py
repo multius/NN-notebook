e9c9d9d49d92660d3e4cdf988fb4108b809b59fd
Multi-Illusion <54984479+Multi-Illusion@users.noreply.github.com>
2022-02-19 18:36:51 +0800

    Update model_learing.py

--- RL/CartPole/model_learing.py
+++ RL/CartPole/model_learing.py
@@ -6,6 +6,6 @@ dqn = DQN.load(model_make.full_path)
 env = lib.Environment()
 
 for i in range(5):
-    dqn.run_in(env, times = 100, need_history = False)
+    dqn.run_in(env, times = 100, need_history = False, need_window = False)
 
     dqn.save(model_make.full_path)
